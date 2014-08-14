5cf2f24c792ae9858db6881d9d01c4d974e95e89
Itamar Syn-Hershko <itamar@code972.com>
2014-08-14 16:41:53 +0300

    Adding isengard-kafka

---
+++ isengard-kafka/attributes/default.rb
@@ -0,0 +1,5 @@
+default['java']['jdk_version'] = '7'
+default['java']['install_flavor'] = 'oracle'
+default['java']['oracle']['accept_oracle_download_terms'] = true
+kafka.install_method = :source
+
