51029508d161902a18bb8d407f5c5a7512dc8a1d
Itamar Syn-Hershko <itamar@code972.com>
2014-08-14 15:25:38 +0300

    Update metadata.rb

--- isengard-zookeeper/metadata.rb
+++ isengard-zookeeper/metadata.rb
@@ -1,7 +1,8 @@
 name             'isengard-zookeeper'
-maintainer       'YOUR_COMPANY_NAME'
+maintainer       'synhershko'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures isengard-zookeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+name             'java'
