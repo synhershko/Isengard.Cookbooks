a047a0b3c5036dec6d4c6d1f53126c22eeaf5a0f
Itamar Syn-Hershko <itamar@code972.com>
2014-08-14 15:07:57 +0300

    Update default.rb

--- isengard-zookeeper/recipes/default.rb
+++ isengard-zookeeper/recipes/default.rb
@@ -1,5 +1,2 @@
-default['java']['jdk_version'] = '7'
-default['java']['install_flavor'] = 'oracle'
-default['java']['oracle']['accept_oracle_download_terms'] = true
 include_recipe 'java::default'
 include_recipe 'hadoop::zookeeper_server'
