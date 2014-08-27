c1b4476ee3e6326ffff662e1f8d7df33cda485ee
Itamar Syn-Hershko <itamar@code972.com>
2014-08-27 11:15:57 +0300

    Update metadata.rb

--- isengard-kafka/metadata.rb
+++ isengard-kafka/metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures isengard-kafka'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends		 'java'
-depends    'kafka'
+depends		 'kafka'
