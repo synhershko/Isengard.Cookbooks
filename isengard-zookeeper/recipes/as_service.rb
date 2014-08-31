6470a6d2f7534d67191fe5c73bde50a3073575e5
Itamar Syn-Hershko <itamar@code972.com>
2014-08-31 17:23:24 +0300

    Support for running ZK as a service

---
+++ isengard-zookeeper/recipes/as_service.rb
@@ -0,0 +1,8 @@
+# download and install base package
+include_recipe 'zookeeper::install'
+
+# set config path and render config
+include_recipe 'zookeeper::config_render'
+
+# start as a service
+include_recipe "zookeeper::service"
