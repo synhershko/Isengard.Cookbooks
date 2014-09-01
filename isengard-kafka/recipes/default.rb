4cdd6bffc11428e23b0ce982afb527c6780c0c03
Itamar Syn-Hershko <itamar@code972.com>
2014-09-01 22:44:57 +0300

    Support verifying Kafka was registered with the ZK ensemble

--- isengard-kafka/recipes/default.rb
+++ isengard-kafka/recipes/default.rb
@@ -32,3 +32,28 @@ node.override[:kafka][:automatic_restart] = true
 
 include_recipe 'kafka'
 
+# ensure the Kafka broker is registered with ZK
+ruby_block "kafkaup" do
+  i = 0
+  block do
+    brokerpath="/brokers/ids/#{node[:kafka][:broker_id]}"
+    zk_host = node[:kafka][:zookeeper][:connect].map{|zkh| "#{zkh}:2181"}.join(",")
+    Chef::Log.info("Zookeeper hosts are #{zk_host}")
+    sleep_time = 0.5
+    kafka_in_zk = znode_exists?(brokerpath, zk_host)
+    while !kafka_in_zk
+      kafka_in_zk = znode_exists?(brokerpath, zk_host)
+      if !kafka_in_zk and i < 20
+        sleep(sleep_time)
+        i += 1
+        Chef::Log.info("Kafka server having znode #{brokerpath} is down.")
+      elsif !kafka_in_zk and i >= 19 
+        Chef::Application.fatal! "Kafka is reported down for more than #{i * sleep_time} seconds"
+      else
+        Chef::Log.info("Broker #{brokerpath} existance : #{znode_exists?(brokerpath, zk_host)}")
+      end
+    end
+    Chef::Log.info("Kafka with znode #{brokerpath} is up and running.")
+  end
+  action :run
+end
