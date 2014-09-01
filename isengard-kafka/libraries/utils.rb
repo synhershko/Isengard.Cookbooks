6c5201ab4da088cb9911e7941491a454704a10d7
Itamar Syn-Hershko <itamar@code972.com>
2014-09-01 16:29:09 +0300

    Kafka cookbook to look for ZK ensemble

---
+++ isengard-kafka/libraries/utils.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: kafka-bcpc
+# Library:: utils
+#
+# Copyright 2013, Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# The method GET_ZK_NODES searches for Zookeeper nodes at two levels (Run List and Roles).
+# During a chef-client run a run list is updated before the chef-client run and is available for 
+# searching nodes. Roles and recipes are updated after the chef-client run completes and commits 
+# data back to the chef-server
+
+def get_zk_nodes
+  rl_results = search(:node, "role:Kafka-Head-Zookeeper AND chef_environment:#{node.chef_environment}")
+  rl_results.map!{|x| x[:hostname] == node[:hostname] ? node : x}
+  ro_results = search(:node, "roles:Kafka-Head-Zookeeper AND chef_environment:#{node.chef_environment}")
+  ro_results.map!{|x| x[:hostname] == node[:hostname] ? node : x}
+  results = rl_results.concat ro_results
+  return results.uniq{|x| float_host(x[:hostname])}.sort
+end
--- isengard-kafka/recipes/default.rb
+++ isengard-kafka/recipes/default.rb
@@ -1,3 +1,29 @@
+# Java version etc is determined and configured via attributes
 include_recipe 'java'
+
+# Get Kafka Zookeeper servers
+#zk_hosts = get_zk_nodes # get by roles
+
+# get by opsworks layers (http://docs.aws.amazon.com/opsworks/latest/userguide/attributes-json-opsworks-layers.html)
+zk_hosts = Array.new
+node[:opsworks][:layers]['zookeeper'][:instances].each do |k,v|
+  Chef::Log.debug(v)
+  Chef::Log.debug(v[:public_dns_name])
+  zk_hosts << v[:public_dns_name]
+end
+
+# Override Kafka related node attributes
+node.override[:kafka][:zookeeper][:connect] = zk_hosts.map{|x| float_host(x[:hostname])}
+#node.override[:kafka][:base_url] = get_binary_server_url + "kafka"
+node.override[:kafka][:host_name] = float_host(node[:fqdn])
+node.override[:kafka][:advertised_host_name] = float_host(node[:fqdn])
+node.override[:kafka][:advertised_port] = 9092
+#node.override[:kafka][:jmx_port] = node[:bcpc][:hadoop][:kafka][:jmx][:port]
+node.override[:kafka][:automatic_start] = true
+node.override[:kafka][:automatic_restart] = true
+
+# Override Zookeeper related node attributes
+#node.override[:bcpc][:hadoop][:zookeeper][:servers] = zk_hosts
+
 include_recipe 'kafka'
 
