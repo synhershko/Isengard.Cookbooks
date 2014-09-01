84b1f10ad846947ecad1afe1b84cc247e24ed825
Itamar Syn-Hershko <itamar@code972.com>
2014-09-01 16:47:04 +0300

    Set swappiness to 0 for kafka instances

--- isengard-kafka/recipes/default.rb
+++ isengard-kafka/recipes/default.rb
@@ -1,6 +1,11 @@
 # Java version etc is determined and configured via attributes
 include_recipe 'java'
 
+# set vm.swapiness to 0 (to lessen swapping)
+sysctl_param 'vm.swappiness' do
+  value 0
+end
+
 # Get Kafka Zookeeper servers
 #zk_hosts = get_zk_nodes # get by roles
 
